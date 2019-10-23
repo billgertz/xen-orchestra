58a73adbfd0903300329426ff845b70b5bd6f5d8
Nicolas Raynaud <github@nraynaud.com>
2019-10-23 20:46:19 +0200

    read and retrieve the grain table on the browser and use it server side  - this allows for reading both streamOptimized and normal VMDK

--- packages/xo-vmdk-to-vhd/src/vmdk-to-vhd.integ.spec.js
+++ packages/xo-vmdk-to-vhd/src/vmdk-to-vhd.integ.spec.js
@@ -67,7 +67,8 @@ test('VMDK to VHD can convert a random data file with VMDKDirectParser', async (
     const result = await readVmdkGrainTable(createFileAccessor(vmdkFileName))
     const pipe = (await convertFromVMDK(
       createReadStream(vmdkFileName),
-      result
+      result.blockAddressList,
+      result.grainsAddressList
     )).pipe(createWriteStream(vhdFileName))
     await eventToPromise(pipe, 'finish')
     await execa('vhd-util', ['check', '-p', '-b', '-t', '-n', vhdFileName])
