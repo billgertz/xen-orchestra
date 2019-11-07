14bbdc95229228001da1b6f252e2f0f28d603e86
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2019-11-07 10:22:37 +0100

    feat(xo-web/backup/new): ability to add new tag in the smart mode

--- packages/xo-web/src/common/intl/messages.js
+++ packages/xo-web/src/common/intl/messages.js
@@ -499,6 +499,7 @@ const messages = {
   deleteOldBackupsFirst: 'Delete first',
   deleteOldBackupsFirstMessage:
     'Delete old backups before backing up the VMs. If the new backup fails, you will lose your old backups.',
+  addTag: 'Add tag',
 
   // ------ New Remote -----
   newRemote: 'New file system remote',
